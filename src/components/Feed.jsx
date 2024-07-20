deaec831b92bc4c8acc5a492298b111c831fd374
Manish kumar <tejnarayan1919@gmail.com>
2024-07-20 17:45:58 +0530

    complete profile

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -4,16 +4,18 @@ import MiniProfile from './MiniProfile'
 
 export default function Feed() {
   return (
-    <main className='grid grid-cols-1 md:grid-cols-3 md:max-w-7xl mx-auto'>
+    <main className="grid grid-cols-1 md:grid-cols-3 md:max-w-7xl mx-auto">
       {/**for post section */}
-      <section className='md:col-span-2'>
+      <section className="md:col-span-2">
         <Post />
       </section>
 
       {/***for mini profile **/}
-      <section className='hidden md:inline-grid md:col-span-1'>
-        <MiniProfile />
+      <section className="hidden md:inline-grid md:col-span-1">
+        <div className='fixed w-[427px]'>
+          <MiniProfile />
+        </div>
       </section>
     </main>
-  )
+  );
 }
--- src/components/MiniProfile.jsx
+++ src/components/MiniProfile.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function MiniProfile() {
+  const { data: session } = useSession();
+  console.log(session);
   return (
-    <div>
-      Profile
+    <div className="flex items-center justify-between mt-14 w-full">
+      <div className="flex items-center gap-5 ml-10">
+        <img
+          src={session?.user?.image || "/verified.png"}
+          alt="user-profile-pic or post-gram logo"
+          className="w-16 h-16 rounded-full border p-[2px]"
+        />
+        <div>
+          <h3 className="text-normal text-blue-800 font-semibold">
+            {session?.user?.username}
+          </h3>
+          <span className="text-base text-gray-500">Welcome to Tick</span>
+        </div>
+      </div>
+
+      {session ? (
+        <button
+          onClick={signOut}
+          className="text-red-500 text-sm font-semibold"
+        >
+          Sign Out
+        </button>
+      ) : (
+        <button
+          onClick={signIn}
+          className="text-blue-500 text-sm font-semibold"
+        >
+          Sign In
+        </button>
+      )}
     </div>
-  )
+  );
 }
